d7315a72fc9b4baa343e63ac241b60ac57a36ba5
JeffreyIhesinulo <jeffriihesinulo@gmail.com>
2025-09-25 13:56:55 +0100

    Add files via upload

--- Lab1/Thermometer.java
+++ Lab1/Thermometer.java
@@ -4,10 +4,14 @@
 public class Thermometer    
 {
      // begin Thermometer
-private double celsius ;
+private double celsius;
  // celsius is accessible to all methods in this class (moreabout private later )
 public Thermometer(){ // constructor method
      setCelsius (0);
+}
+public Thermometer(double cel)
+{
+     setCelsius(cel);
 }
 public void setCelsius (double cel ){ // method to set the temperature
 celsius = cel;
