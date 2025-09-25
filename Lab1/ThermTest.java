0b29f5a8bc6492ae24aa7268fed518cd18726c82
JeffreyIhesinulo <jeffriihesinulo@gmail.com>
2025-09-25 13:57:31 +0100

    Add files via upload

--- Lab1/ThermTest.java
+++ Lab1/ThermTest.java
@@ -8,8 +8,7 @@ Thermometer thermA = new Thermometer();// Create an instance of Thermometer clas
 System.out.println ("Temp. of Thermometer A is " + thermA.getCelsius() );
 thermA.setCelsius (20.0) ;
 System.out.println ("Temp. of Thermometer A is " + thermA.getCelsius() );
-Thermometer thermB = new Thermometer();// Create an instance of Thermometer class
-thermB.setCelsius(10);
+Thermometer thermB = new Thermometer(10);// Create an instance of Thermometer class
 System.out.println("Temp. of Thermometer B is " + thermB.getCelsius());
 } // end main
 } // end class ThermT    
